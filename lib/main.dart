109ae6c20c2f87208d3264508f594b51b3008425
Dipu Ray <dipuray7779@gmail.com>
2025-05-08 01:32:21 +0600

    Changes to be committed: 	modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Block App',
+      title: 'Blog App',
       theme: AppTheme.darkThemeMode,
       home: const LoginPage(),
     );
